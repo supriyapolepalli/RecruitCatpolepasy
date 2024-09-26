171cbbf6b5dae06a08a51367d06fdd2f9abb4a4b
Supriya Polepalli <39058333+supriyapolepalli@users.noreply.github.com>
2024-09-26 14:44:35 -0400

    Your descriptive commit message

--- Pages/Models/Candidate.cs
+++ Pages/Models/Candidate.cs
@@ -1,17 +1,63 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
 namespace RecruitCatpolepasy.Models
 {
     public class Candidate
     {
-        public int Id { get; set; }  // Primary Key
-        public string FirstName { get; set; } // Candidate's First Name
-        public string LastName { get; set; } // Candidate's Last Name
-        public decimal TargetSalary { get; set; } // Candidate's Target Salary
-        public DateTime? StartDate { get; set; } // Optional Start Date
-        public int JobTitleId { get; set; }  // Foreign Key to JobTitle
-        public JobTitle JobTitle { get; set; } // Navigation property to JobTitle
-        public int CompanyId { get; set; }  // Foreign Key to Company
-        public Company Company { get; set; } // Navigation property to Company
+        public int Id { get; set; } 
+         [Required]
+    [StringLength(50)]
+        [DisplayName ("First Name ")]
+ 
+        public string FirstName { get; set; }
+         [Required]
+    [StringLength(50)]
+[DisplayName ("Last Name ")]
+        public string LastName { get; set; } 
+         [Required]
+    [StringLength(50)]
+        [DisplayName ("Target Salary ")]
+        public decimal TargetSalary { get; set; } 
+ [Required]
+    [StringLength(10)]
+        [DisplayName ("Start Date ")]
+        public DateTime? StartDate { get; set; } 
+         [Required]
+    [StringLength(50)]
+        [DisplayName ("Job Title ID ")]
+        
+        public int JobTitleId { get; set; } 
+         [Required]
+    [StringLength(50)]
+        [DisplayName ("Job Title ")]
+        public JobTitle JobTitle { get; set; } 
+         [Required]
+    [StringLength(50)]
+        [DisplayName ("Company ID ")]
+   
+        public int CompanyId { get; set; }  
+         [Required]
+    [StringLength(50)]
+        [DisplayName ("Company ")]
+        public Company Company { get; set; } 
+         [Required]
+    [StringLength(50)]
+        [DisplayName ("Industry ID ")]
         public int IndustryId { get; set; }  // Foreign Key to Industry
+         [Required]
+    [StringLength(50)]
+        [DisplayName ("Industry ")]
         public Industry Industry { get; set; } // Navigation property to Industry
+        [EmailAddress]
+public string Email { get; set; }
+
+[Phone]
+public string PhoneNumber { get; set; }
+
+[Url]
+public string Website { get; set; }
+public int Age { get; set; }
+public DateTime BirthDate { get; set; }
     }
 }
--- obj/Debug/net8.0/RecruitCatpolepasy.AssemblyInfo.cs
+++ obj/Debug/net8.0/RecruitCatpolepasy.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RecruitCatpolepasy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f8672c3b8241ed103e6d8e5f8e006d06c8fa772")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0537d3a8bd2f7aba66bf9bf5f0412b12c29df4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("RecruitCatpolepasy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RecruitCatpolepasy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
